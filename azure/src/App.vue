38d4c6a6412f52b670900d9b916d5e049c22a921
gemu <g3mu@hotmail.com>
2020-05-05 17:20:37 -0400

    u

---
+++ azure/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      hello: '- no data -',
+    }
+  },
+
+  methods: {
+    async helloWorld() {
+      try {
+        const res = await axios.get('http://127.0.0.1:5000/hello')
+        console.debug(res)
+        this.hello = res.data
+      } catch (e) {
+        console.error(e)
+      }
+    },
+  },
+
+  created() {
+    this.helloWorld();
+  }
+}
+</script>
